c1f002b9bde95a2fdd49aee057394f543f01c6b0
Yuna <7baeyuna@gmail.com>
2025-04-05 19:55:00 +1300

    first commit

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   const root = ReactDOM.createRoot(div);
   root.render(<App />);
 });
 
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
+describe("Addition", () => {
+  it("knows that 2 and 2 make 4", () => {
     expect(2 + 2).toBe(4);
   });
 });
-
